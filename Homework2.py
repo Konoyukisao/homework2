5c16fbfbbbbc97defd835296dd46aed72a176521
Konoyukisao <163278889+Konoyukisao@users.noreply.github.com>
2024-03-18 01:44:05 +0700

    Add files via upload

---
+++ Homework2.py
@@ -0,0 +1,7 @@
+name = 'Sergei'
+print("Name: ", name, type(name))
+age = 19
+print("Age: ", age, type(age))
+print("New age: ", age + 1, type(age + 1))
+is_student = True
+print("Is Student: ", is_student, type(is_student))
